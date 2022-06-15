de2beb957fdbac1bb051477801e745eea866a1cc
MatteoGenovese <matteo.genovese@icloud.com>
2022-06-15 15:25:02 +0200

    milestone 1

--- js/script.js
+++ js/script.js
@@ -1,37 +1,47 @@
-const numKm = parseInt( prompt("Inserisci il numero di chilometri"));
-const etaPasseggero = parseInt( prompt("Inserire l'età"));
+let numKm = NaN;
+let etaPasseggero =  NaN;
 const prezzoKm = 0.26;
 const scontoMinorenni=0.15;
 const scontoOver65=0.35;
 
+var submitButton= document.getElementById(`submit-button`);
 
-if(isNaN(numKm)){   //controllo che sia un numero
-    console.log(`non è stato inserito un valore numerico per il numero di chilometri`);
-}
-else if(numKm<0){   //controllo che il numero di chilometri non sia negativo    
-    console.log(`il numero di Km non può essere negativo`);
-}
-else{               //passo ai controlli di età 
-    if( isNaN( etaPasseggero)){     //controllo che sia un numero
-        console.log(`non è stato inserito un valore numerico per l'età`);
+submitButton.addEventListener(`click`,
+    function(){
+        let numKm = document.querySelector(`#km-num-input`).value;
+        let etaPasseggero =  document.querySelector(`#age-input`).value;
+
+        if(isNaN(numKm)){   //controllo che sia un numero
+            document.getElementById('show-price').innerHTML=`non è stato inserito un valore numerico per il numero di chilometri`;
+        }
+        else if(numKm<0){   //controllo che il numero di chilometri non sia negativo    
+            document.getElementById('show-price').innerHTML=`il numero di Km non può essere negativo`;
+        }
+        else{               //passo ai controlli di età 
+            if( isNaN( etaPasseggero)){     //controllo che sia un numero
+                document.getElementById('show-price').innerHTML=`non è stato inserito un valore numerico per l'età`;
+            }
+            else if(etaPasseggero<0){       //controllo che non sia negativo
+                document.getElementById('show-price').innerHTML=`l'età non può essere negativa`;
+            } else {
+                prezzoBiglietto=(numKm*prezzoKm).toFixed(2); //calcolo il prezzo a prescindere
+                if(etaPasseggero<18){           //calcolo il prezzo scontato ai minori
+                    prezzoBigliettoScontato = (prezzoBiglietto - prezzoBiglietto*scontoMinorenni).toFixed(2);    //toFixed(num) metodo che arrotonda a num numeri decimali una variabile
+                    document.getElementById('show-price').innerHTML=`totale tratta con sconto minorenni: ${prezzoBigliettoScontato} euro`;
+                }
+                else if(etaPasseggero>65){      //calcolo il prezzo scontato per gli over 65
+                    prezzoBigliettoScontato = (prezzoBiglietto - prezzoBiglietto*scontoOver65).toFixed(2);
+                    document.getElementById('show-price').innerHTML=`totale tratta con sconto over 65: ${prezzoBigliettoScontato} euro`;
+                }
+                else{                           //calcolo il prezzo del biglietto intero
+                    document.getElementById('show-price').innerHTML=`totale biglietto intero: ${prezzoBiglietto} euro`;
+                }
+                    
+                
+                
+            }
+        }
     }
-    else if(etaPasseggero<0){       //controllo che non sia negativo
-        console.log(`l'età non può essere negativa`);
-    } else {
-        prezzoBiglietto=(numKm*prezzoKm).toFixed(2); //calcolo il prezzo a prescindere
-        if(etaPasseggero<18){           //calcolo il prezzo scontato ai minori
-            prezzoBigliettoScontato = (prezzoBiglietto - prezzoBiglietto*scontoMinorenni).toFixed(2);    //toFixed(num) metodo che arrotonda a num numeri decimali una variabile
-            console.log(`totale tratta con sconto minorenni: ${prezzoBigliettoScontato} euro`);
-        }
-        else if(etaPasseggero>65){      //calcolo il prezzo scontato per gli over 65
-            prezzoBigliettoScontato = (prezzoBiglietto - prezzoBiglietto*scontoOver65).toFixed(2);
-            console.log(`totale tratta con sconto over 65: ${prezzoBigliettoScontato} euro`);
-        }
-        else{                           //calcolo il prezzo del biglietto intero
-            console.log(`totale biglietto intero: ${prezzoBiglietto} euro`);
-        }
-            
-        
-        
-    }
-}
+)
+
+
