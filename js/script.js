fdba6b6d389ac25b54b803f2cfbe3cc7af75e8fb
MatteoGenovese <matteo.genovese@icloud.com>
2022-06-15 17:40:45 +0200

    show ticket or show message

--- js/script.js
+++ js/script.js
@@ -1,44 +1,74 @@
-let numKm = NaN;
-let etaPasseggero =  NaN;
+let prezzoBigliettoScontato=0;
+let prezzoBiglietto=0;
 const prezzoKm = 0.26;
 const scontoMinorenni=0.15;
 const scontoOver65=0.35;
 
 var submitButton= document.getElementById(`submit-button`);
-
 submitButton.addEventListener(`click`,
     function(){
+        document.getElementById('show-message').innerHTML=``;
         let numKm = document.querySelector(`#km-num-input`).value;
-        let etaPasseggero =  document.querySelector(`#age-input`).value;
+        let ticketType = parseInt( document.querySelector(`#ticket-type`).value);
+        let userName = document.querySelector(`#insert-name`).value;
+        let userTicket = document.getElementById(`your-ticket`);
+        let messageForUser = document.getElementById(`show-message`);
 
-        if(isNaN(numKm)){   //controllo che sia un numero
-            document.getElementById('show-price').innerHTML=`non è stato inserito un valore numerico per il numero di chilometri`;
+
+        // console.log(typeof ticketType);
+
+        if(numKm==0){   //controllo che sia un numero
+            messageForUser.classList.remove(`visually-hidden`);
+            document.getElementById('show-message').innerHTML=`non è stato inserito un valore numerico per il numero di chilometri`;
         }
         else if(numKm<0){   //controllo che il numero di chilometri non sia negativo    
-            document.getElementById('show-price').innerHTML=`il numero di Km non può essere negativo`;
+            messageForUser.classList.remove(`visually-hidden`);
+            document.getElementById('show-message').innerHTML=`il numero di Km non può essere negativo`;
         }
-        else{               //passo ai controlli di età 
-            if( isNaN( etaPasseggero)){     //controllo che sia un numero
-                document.getElementById('show-price').innerHTML=`non è stato inserito un valore numerico per l'età`;
-            }
-            else if(etaPasseggero<0){       //controllo che non sia negativo
-                document.getElementById('show-price').innerHTML=`l'età non può essere negativa`;
-            } else {
-                prezzoBiglietto=(numKm*prezzoKm).toFixed(2); //calcolo il prezzo a prescindere
-                if(etaPasseggero<18){           //calcolo il prezzo scontato ai minori
+        else{               
+            switch(ticketType) {
+                case 0:
+                    // code block
+                    messageForUser.classList.remove(`visually-hidden`);
+                    document.getElementById('show-message').innerHTML=`Selezionare la tipologia di biglietto`;
+                    break;
+                case 1:
+                  // code block
+                    prezzoBiglietto=prezzoKm*numKm;
                     prezzoBigliettoScontato = (prezzoBiglietto - prezzoBiglietto*scontoMinorenni).toFixed(2);    //toFixed(num) metodo che arrotonda a num numeri decimali una variabile
-                    document.getElementById('show-price').innerHTML=`totale tratta con sconto minorenni: ${prezzoBigliettoScontato} euro`;
-                }
-                else if(etaPasseggero>65){      //calcolo il prezzo scontato per gli over 65
-                    prezzoBigliettoScontato = (prezzoBiglietto - prezzoBiglietto*scontoOver65).toFixed(2);
-                    document.getElementById('show-price').innerHTML=`totale tratta con sconto over 65: ${prezzoBigliettoScontato} euro`;
-                }
-                else{                           //calcolo il prezzo del biglietto intero
-                    document.getElementById('show-price').innerHTML=`totale biglietto intero: ${prezzoBiglietto} euro`;
-                }
+                    userTicket.classList.remove(`visually-hidden`);
+
+
+                    document.getElementById('your-name').innerHTML+=` ${userName}`;
+                    document.getElementById('show-price').innerHTML+=` ${prezzoBigliettoScontato}`;
+                    document.getElementById('km-counted').innerHTML+=` ${numKm}`;
+                    document.getElementById('ticket-type-2').innerHTML+=` Minorenne`;
+                    break;
+
+                case 2:
+                    prezzoBiglietto=(numKm*prezzoKm).toFixed(2); //calcolo il prezzo a prescindere
+
+                    userTicket.classList.remove(`visually-hidden`);
+
+
+                    document.getElementById('your-name').innerHTML+=` ${userName}`;
+                    document.getElementById('show-price').innerHTML+=` ${prezzoBiglietto}`;
+                    document.getElementById('km-counted').innerHTML+=` ${numKm}`;
+                    document.getElementById('ticket-type-2').innerHTML+=` Intero`;
                     
-                
-                
+                    break;
+                case 3:
+                  // code block
+                    prezzoBiglietto=prezzoKm*numKm;
+                    prezzoBigliettoScontato = (prezzoBiglietto - prezzoBiglietto*scontoOver65).toFixed(2);
+
+                    userTicket.classList.remove(`visually-hidden`);
+
+                    document.getElementById('your-name').innerHTML+=` ${userName}`;
+                    document.getElementById('show-price').innerHTML+=` ${prezzoBigliettoScontato}`;
+                    document.getElementById('km-counted').innerHTML+=` ${numKm}`;
+                    document.getElementById('ticket-type-2').innerHTML+=` Over 65`;
+                    break;
             }
         }
     }
